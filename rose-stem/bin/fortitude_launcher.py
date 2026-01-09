7a4cb6c9be090eb91461dd2a17a0614dd35b98d4
mo-lucy-gordon <lucy.gordon@metoffice.gov.uk>
2026-01-09 17:42:55 +0000

    added new files

---
+++ rose-stem/bin/fortitude_launcher.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+##############################################################################
+# (c) Crown copyright 2026 Met Office. All rights reserved.
+# The file LICENCE, distributed with this code, contains details of the terms
+# under which the code may be used.
+##############################################################################
+
+"""
+Launch fortitude on list of directories. Run on all and print outputs. Fail if
+any style changes required.
+"""
+
+import argparse
+import os
+import subprocess
+import sys
+
+def launch_fortitude(config_path, app_path):
+    """
+    Launch fortitude as a subprocess command and check the output
+    """
+
+    command = f"fortitude --config-file {config_path} check {app_path}"
+    result = subprocess.run(command.split(), capture_output=True, text=True)
+
+    print(result.stdout)
+    return result
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Run fortitude on all applications. If "
+        "application/fortitude.toml exists use that file, otherwise "
+        "use one in rose-stem/app/check_fortitude_linter/file. "
+        "Print output, raise error if any changes required."
+    )
+    parser.add_argument(
+        "-s",
+        "--source",
+        help="The top level of lfric_core directory.",
+        required=True,
+    )
+    args = parser.parse_args()
+
+    failed_apps = {}
+
+    candidates = [
+        "infrastructure",
+        "mesh_tools",
+        "components/coupling",
+        "components/driver",
+        "components/science",
+        "components/inventory",
+        "components/lfric-xios",
+        "applications/skeleton",
+        "applications/simple_diffusion",
+        "applications/io_demo",
+    ]
+    for app in candidates:
+        print(f"Running on {app}\n")
+        app_path = os.path.join(args.source, app)
+        config_path = os.path.join(app_path, "fortitude.toml")
+        if not os.path.exists(os.path.join(config_path)):
+            print("Using universal config (toml) file. (Some apps use their own config file.)")
+            config_path = os.path.join(
+                args.source,
+                "rose-stem",
+                "app",
+                "check_fortitude_linter",
+                "file",
+                "fortitude.toml",
+            )
+        result = launch_fortitude(config_path, app_path)
+        if result.returncode:
+            print(f"Checking: {app} \n", file=sys.stderr) #prints the app run on if there are errors of any kind
+            if not result.stderr:
+                print("Found lint errors:", file=sys.stderr)# prints if no other/config errors are found
+                print(result.stdout, file=sys.stderr)    #prints the lint errors
+            if result.stderr:
+                print("Found non-lint errors: \n", file=sys.stderr) #prints if there are other/config errors
+                print(result.stderr, "\n\n\n", file=sys.stderr) #prints the other/config error
+            failed_apps[app] = result.stderr
+
+    if failed_apps:
+        error_message = ""
+        print("\n\n\nSummary: Fortitude found errors in the following repositories:\n", file=sys.stderr)
+        for failed in failed_apps:
+            error_message += f"{failed}\n"
+        sys.exit(error_message)
