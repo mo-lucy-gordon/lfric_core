439ee4e6065a934f11a6844365049b14d117fb82
mo-lucy-gordon <lucy.gordon@metoffice.gov.uk>
2026-03-11 17:35:56 +0000

    updated script to be universal, added code review suggestions

--- rose-stem/bin/fortitude_launcher.py
+++ rose-stem/bin/fortitude_launcher.py
@@ -6,23 +6,23 @@
 ##############################################################################
 
 """
-Launch fortitude on list of directories. Run on all and print outputs. Fail if
-any style changes required.
+ Launch fortitude on list of directories. Run on all and print outputs.
+Fail if any style changes required.
 """
 
-import argparse
-import os
-import subprocess
 import sys
+import subprocess
+import argparse
+from pathlib import Path
 
 
-def launch_fortitude(config_path, app_path):
+def launch_fortitude(config_path: Path, app_path: Path) -> subprocess.CompletedProcess[str]:
     """
     Launch fortitude as a subprocess command and check the output
     """
 
-    command = f"fortitude --config-file {config_path} check {app_path}"
-    result = subprocess.run(command.split(), capture_output=True, text=True)
+    command: list[str] = ["fortitude", "--config-file", str(config_path), "check", str(app_path)]
+    result = subprocess.run(command, capture_output=True, text=True)
 
     print(result.stdout)
     return result
@@ -36,61 +36,47 @@ if __name__ == "__main__":
         "Print output, raise error if any changes required."
     )
     parser.add_argument(
-        "-s",
-        "--source",
-        help="The top level of lfric_core directory.",
-        required=True,
+        "source",
+        help="The top level of lfric_apps directory."
     )
     args = parser.parse_args()
 
-    failed_apps = {}
+    source_path: Path = Path(args.source)
 
-    candidates = [
-        "infrastructure",
-        "mesh_tools",
-        "components/coupling",
-        "components/driver",
-        "components/science",
-        "components/inventory",
-        "components/lfric-xios",
-        "applications/skeleton",
-        "applications/simple_diffusion",
-        "applications/io_demo",
-    ]
-    for app in candidates:
-        print(f"Running on {app}\n")
-        app_path = os.path.join(args.source, app)
-        config_path = os.path.join(app_path, "fortitude.toml")
-        if not os.path.exists(os.path.join(config_path)):
-            print("Using universal config (toml) file."
+    failed_apps: dict[str, str] = {}
+
+    for top_dir_path in source_path.iterdir():   #e.g. applications,science,interfaces
+        if not top_dir_path.is_dir():  # don't try to loop over files
+            continue
+        for app_path in top_dir_path.iterdir():   #e.g. adjoint_tests, adjoint, coupled_interface
+            if not app_path.is_dir():
+                continue
+            app_name: str = app_path.name
+            print(f"Running on {app_name}\n")
+            config_path: Path = app_path/"fortitude.toml"
+            if not config_path.exists():
+                print("Using universal config (toml) file."
                   " (Some apps use their own config file.)")
-            config_path = os.path.join(
-                args.source,
-                "rose-stem",
-                "app",
-                "check_fortitude_linter",
-                "file",
-                "fortitude.toml",
-            )
+                config_path: Path = (source_path / "rose-stem" / "app" / "check_fortitude_linter" / "file" / "fortitude.toml")
+            result: subprocess.CompletedProcess[str] = launch_fortitude(config_path, app_path)
+            if result.returncode:
+                # prints the app run on if there are errors of any kind
+                print(f"Checking: {app_name} \n", file=sys.stderr)
+                if not result.stderr:
+                    # prints if no other/config errors are found
+                       print("Found lint errors:", file=sys.stderr)
+                    # prints the lint errors
+                       print(result.stdout, file=sys.stderr)
+                if result.stderr:
+                    # prints if there are other/config errors
+                       print("Found non-lint errors: \n", file=sys.stderr)
+                    # prints the other/config errors
+                       print(result.stderr, "\n\n\n", file=sys.stderr)
+                failed_apps[app_name] = result.stderr
 
-        result = launch_fortitude(config_path, app_path)
-        if result.returncode:
-            # prints the app run on if there are errors of any kind
-            print(f"Checking: {app} \n", file=sys.stderr)
-            if not result.stderr:
-                # prints if no other/config errors are found
-                print("Found lint errors:", file=sys.stderr)
-                # prints the lint errors
-                print(result.stdout, file=sys.stderr)
-            if result.stderr:
-                # prints if there are other/config errors
-                print("Found non-lint errors: \n", file=sys.stderr)
-                # prints the other/config error
-                print(result.stderr, "\n\n\n", file=sys.stderr)
-            failed_apps[app] = result.stderr
 
     if failed_apps:
-        error_message = ""
+        error_message: str = ""
         print("\n\n\nSummary: Fortitude found errors in"
               " the following repositories:\n", file=sys.stderr)
         for failed in failed_apps:
